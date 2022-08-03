c621d543e621898a1da071205e317803718692fb
egostigma <mustopamahmud@icloud.com>
2022-08-03 08:13:05 +0700

    fix: family tree loop

--- resources/views/components/child-tree.blade.php
+++ resources/views/components/child-tree.blade.php
@@ -9,17 +9,7 @@
         </div>
         <div class="{{ $child->children_count ? '_new-branch' : ''}} d_f">
             @foreach ($child->children()->withCount("children")->get() as $child2)
-                <div class="{{ $child->children_count ? '_tree-root' : ''}} d_f">
-                    <div class="_tree-branch {{ $child2->children_count ? 'has-children' : ''}}">
-                        <div class="_tree-raw d_f">
-                            <div class="_tree-leaf d_f">
-                                <div class="t_data g_{{ $child2->gender }} d_f" onclick="location.href = '{{ route('family-member.show', $child2->id) }}'">
-                                    <p class="short-name">{{ $child2->name }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('components.child-tree', ["parent" => $child, "child" => $child2])
             @endforeach
         </div>
     </div>
